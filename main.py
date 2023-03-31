705bc1b5d6b370e64d6ee81f07f38d56c4df2062
bighammer <3225579752@qq.com>
2023-03-31 18:25:47 +0800

    我自己用，ikuu

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ passwd = os.environ.get('PASSWD')
 # server酱
 SCKEY = os.environ.get('SCKEY')
 
-login_url = 'https://fenda.cloud/auth/login'
-check_url = 'https://fenda.cloud/user/checkin'
-info_url = 'https://fenda.cloud/user/profile'
+login_url = 'https://ikuuu.eu/auth/login'
+check_url = 'https://ikuuu.eu/user/checkin'
+info_url = 'https://ikuuu.eu/user/profile'
 
 header = {
-        'origin': 'https://fenda.cloud',
+        'origin': 'https://ikuuu.eu',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 data = {
@@ -26,20 +26,20 @@ try:
     print(response['msg'])
     # 获取账号名称
     info_html = session.get(url=info_url,headers=header).text
-    info = "".join(re.findall('<span class="user-name text-bold-600">(.*?)</span>', info_html, re.S))
-    print(info)
+#     info = "".join(re.findall('<span class="user-name text-bold-600">(.*?)</span>', info_html, re.S))
+#     print(info)
     # 进行签到
     result = json.loads(session.post(url=check_url,headers=header).text)
     print(result['msg'])
-    content = info+'\n\n'+result['msg']
+    content = result['msg']
     # 进行推送
     if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=fenda签到&desp={}'.format(SCKEY, content)
+        push_url = 'https://sctapi.ftqq.com/{}.send?title=ikuu签到&desp={}'.format(SCKEY, content)
         requests.post(url=push_url)
         print('推送成功')
 except:
     content = '签到失败'
     print(content)
     if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=fenda签到&desp={}'.format(SCKEY, content)
+        push_url = 'https://sctapi.ftqq.com/{}.send?title=ikuu签到&desp={}'.format(SCKEY, content)
         requests.post(url=push_url)

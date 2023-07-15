f8d164fcd477a9df877d7c92706b263856088288
StudentYee <109954988+StudentYee@users.noreply.github.com>
2023-07-15 19:01:15 +0800

    Change Domain

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ passwd = os.environ.get('PASSWD')
 # server酱
 SCKEY = os.environ.get('SCKEY')
 
-login_url = 'https://ikuuu.eu/auth/login'
-check_url = 'https://ikuuu.eu/user/checkin'
-info_url = 'https://ikuuu.eu/user/profile'
+login_url = 'https://ikuuu.art/auth/login'
+check_url = 'https://ikuuu.art/user/checkin'
+info_url = 'https://ikuuu.art/user/profile'
 
 header = {
-        'origin': 'https://ikuuu.eu',
+        'origin': 'https://ikuuu.art',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 data = {

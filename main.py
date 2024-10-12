fe7924a5f917af01aa57410398cca065b556937f
LuoHui666 <54461664+LuoHui666@users.noreply.github.com>
2024-10-12 10:19:52 +0800

    Update main.py
    new url

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def push(content):
 
 # 会不定时更新域名，记得Sync fork
 
-login_url = 'https://ikuuu.me/auth/login'
-check_url = 'https://ikuuu.me/user/checkin'
-info_url = 'https://ikuuu.me/user/profile'
+login_url = 'https://ikuuu.pw/auth/login'
+check_url = 'https://ikuuu.pw/user/checkin'
+info_url = 'https://ikuuu.pw/user/profile'
 
 header = {
         'origin': 'https://ikuuu.me',

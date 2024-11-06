d3cbb8f822aef2aab85349c1a46c230e0659d586
spockch <74373801+spockch@users.noreply.github.com>
2024-11-06 10:43:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ check_url = 'https://ikuuu.pw/user/checkin'
 info_url = 'https://ikuuu.pw/user/profile'
 
 header = {
-        'origin': 'https://ikuuu.pw',
+        'origin': 'https://ikuuu.one',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 
